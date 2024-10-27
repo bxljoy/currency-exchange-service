17ca85351174f0d7e393d9a3ba31d978c51b7cea
bxljoy <259500313@qq.com>
2024-10-27 22:51:30 +0100

    Refactor pom.xml to add dependencies for Spring Cloud Netflix Eureka Client, Resilience4j, OpenFeign, Micrometer, and OpenTelemetry Zipkin exporter

--- src/main/java/com/alex/microservices/currency_exchange_service/controllers/CurrencyExchangeController.java
+++ src/main/java/com/alex/microservices/currency_exchange_service/controllers/CurrencyExchangeController.java
@@ -6,6 +6,8 @@ import com.alex.microservices.currency_exchange_service.domain.CurrencyExchange;
 import com.alex.microservices.currency_exchange_service.services.CurrencyExchangeService;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     private final Environment environment;
     private final CurrencyExchangeService currencyExchangeService;
@@ -28,6 +32,8 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ResponseEntity<CurrencyExchange> retrieveExchangeValue(@PathVariable String from,
             @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         Optional<CurrencyExchange> exchangeValue = currencyExchangeService.retrieveExchangeValue(from, to);
         return exchangeValue.map(currencyExchange -> {
             String port = environment.getProperty("local.server.port");
